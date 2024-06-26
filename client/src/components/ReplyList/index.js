fc7d5eaf0a5195f334157c18dd36f1f61cf4a9a3
Celina <celinalouissaint@gmail.com>
2024-06-25 18:05:07 -0700

    clean up nested p

--- client/src/components/ReplyList/index.js
+++ client/src/components/ReplyList/index.js
@@ -3,24 +3,25 @@ import { Link } from "react-router-dom";
 import Grid from "@material-ui/core/Grid";
 
 const ReplyList = ({ replies }) => {
-  console.log("reply list",replies)
   return (
     <div>
-      <br/>
+      <br />
       <h5>Replies:</h5>
       <div>
         {replies &&
           replies.map((reply) => (
-            <p className="" key={reply._id}>
-              <Link
-                // to={`/profile/${reply.username}`}
-                style={{ fontWeight: 700 }}
-              >
-                {reply.username}
-              </Link>{" "}
-              : {reply.replyBody}
+            <div className="" key={reply._id}>
+              <p>
+                <Link
+                  // to={`/profile/${reply.username}`}
+                  style={{ fontWeight: 700 }}
+                >
+                  {reply.username}
+                </Link>{" "}
+                : {reply.replyBody}
+              </p>
               <div className="messageDate">{reply.createdAt}</div>
-            </p>
+            </div>
           ))}
       </div>
     </div>
